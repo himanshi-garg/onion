8b7a3368e078a21f90a29ab74f045ff3dd200cc5
himanshi-garg <hgar951@gmail.com>
2023-02-03 13:44:01 +1100

    added some files

---
+++ onion/__init__.py
@@ -0,0 +1,3 @@
+__version__ = "1.0"
+
+from .extract_layers import *
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='onion',
+      description='discs have layers',
+      url='https://github.com/himanshi-garg/onion',
+      author='Himanshi Garg',
+      license='MIT',
+      packages=['onion'],
+      zip_safe=False)
